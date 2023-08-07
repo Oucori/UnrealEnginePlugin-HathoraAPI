84929ec68d3ded6803bf4bc04b8646098aef7eb5
Gianni Bischoff <Oucori@gmail.com>
2023-08-07 11:36:00 +0200

    Initial Commit

---
+++ Source/HathoraAPI/Private/HathoraAPI.cpp
@@ -0,0 +1,22 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HathoraAPI.h"
+
+#define LOCTEXT_NAMESPACE "FHathoraAPIModule"
+
+void FHathoraAPIModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+}
+
+void FHathoraAPIModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+	
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FHathoraAPIModule, HathoraAPI)
---
+++ Source/HathoraAPI/Private/HathoraAPIBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HathoraAPIBPLibrary.h"
+#include "HathoraAPI.h"
+
+UHathoraAPIBPLibrary::UHathoraAPIBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UHathoraAPIBPLibrary::HathoraAPISampleFunction(float Param)
+{
+	return -1;
+}
+
